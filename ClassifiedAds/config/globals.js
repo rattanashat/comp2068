03743bbbf81a3c55027c135cf212388d8e4b938e
rattanashat <ratz407@gmail.com>
2017-03-21 23:18:29 -0400

    new

---
+++ ClassifiedAds/config/globals.js
@@ -0,0 +1,14 @@
+/**
+ * Created by apple on 21/03/17.
+ */
+module.exports = {
+    // db: 'mongodb://localhost/comp2068-w2017' // local mongodb
+    db :'w2017dba:root@ds139480.mlab.com:39480/assignment2ads',  // mlab
+
+    facebook:{
+        clientID: '440356099639187',
+        clientSecret:'28196a302514c15a6f2fdd3e1f1ae210',
+        callbackURL: 'http://localhost:3000/facebook/callback'
+
+    }
+};
